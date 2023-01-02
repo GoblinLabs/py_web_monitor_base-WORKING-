5ccf92dc004b10d64f8cc359aa756a744e8c6742
Chevin <chevin@duck.com>
2023-01-02 18:02:11 +0100

    1

--- app.py
+++ app.py
@@ -10,34 +10,38 @@ import time
 import platform
 import math
 import os
-
-
-#TO GET ONCE
-hostname = (socket.gethostname())
-get_local_ip = (socket.gethostbyname(socket.gethostname()))
-get_os_version = platform.platform()
-#get_uptime = os.popen('uptime -p').read()[:-1]
-
-
-#TO UPDATE EVERY TOT SECONDS
-#RAM
-
-get_ram_gb = round(psutil.virtual_memory()[3]/1000000000, 1)
-
-#CPU
-
-#STORAGE
-get_storage_tot = round(psutil.disk_usage('/')[0]/1000000000, 0)
-get_storage_used = round(psutil.disk_usage('/')[1]/1000000000, 0)
-get_storage_free = round(psutil.disk_usage('/')[2]/1000000000, 0)
-
-
+"""
+data = {
+         "ip": socket.gethostbyname(socket.gethostname()),
+         "hostname": socket.gethostname(),
+         "domain": ".lcl",
+         "os_version": platform.platform(),
+         "ram_used": round(psutil.virtual_memory()[3]/1000000000, 1),
+         "storage_free": round(psutil.disk_usage('/')[2]/1000000000, 0),
+         "storage_used": round(psutil.disk_usage('/')[1]/1000000000, 0),
+         "sorage_tot": round(psutil.disk_usage('/')[0]/1000000000, 0),
+         "cpu": ""
+      }
+"""
 
 app = Flask('testapp') 
 
 @app.route('/')
 def index():
-    return render_template('index.html', variable='12345', hostname=hostname, get_local_ip=get_local_ip, get_os_version=get_os_version, get_ram_gb=get_ram_gb, get_storage_used=get_storage_used, get_storage_tot=get_storage_tot)
+    data = {
+         "ip": socket.gethostbyname(socket.gethostname()),
+         "hostname": socket.gethostname(),
+         "domain": ".lcl",
+         "os_version": platform.platform(),
+         "ram_used": round(psutil.virtual_memory()[3]/1000000000, 1),
+         "ram_used_percent": psutil.virtual_memory().percent,
+         "storage_free": round(psutil.disk_usage('/')[2]/1000000000, 0),
+         "storage_used": round(psutil.disk_usage('/')[1]/1000000000, 0),
+         "sorage_tot": round(psutil.disk_usage('/')[0]/1000000000, 0),
+         "cpu": psutil.cpu_percent(interval=None, percpu=False)
+      }
+    
+    return render_template('index.html', refreshrate=5, hostname=data['hostname'], get_local_ip=data['ip'], get_os_version=data['os_version'], get_ram_gb=data['ram_used'], get_storage_used=data['storage_used'], get_storage_tot=data['sorage_tot'], cpu=data['cpu'], ram_used_percent=data['ram_used_percent'])
 
 
 if __name__ == '__main__':
