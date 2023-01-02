88979d97b3d09b7ff11a6b7f27c84c00899dbdd3
Chevin <chevin@duck.com>
2023-01-02 16:40:46 +0100

    2

--- data.py
+++ data.py
@@ -1,5 +1,3 @@
-from flask import Flask, render_template
-
 import time
 import json
 import time
@@ -11,9 +9,11 @@ import platform
 import math
 import os
 
+HOST = "127.0.0.1"
+PORT = 8080
 
 #TO GET ONCE
-hostname = (socket.gethostname())
+get_hostname = (socket.gethostname())
 get_local_ip = (socket.gethostbyname(socket.gethostname()))
 get_os_name = platform.system()
 get_os_version = platform.platform()
@@ -34,14 +34,23 @@ get_storage_tot = round(psutil.disk_usage('/')[0]/1000000000, 0)
 get_storage_used = round(psutil.disk_usage('/')[1]/1000000000, 0)
 get_storage_free = round(psutil.disk_usage('/')[2]/1000000000, 0)
 
+#NETWORK
 
-app = Flask('testapp')
-
-path = 'data.json'
-
-@app.route('/')
-def index():
-    return render_template('index.html', variable='12345', hostname=hostname, path=path, get_local_ip=get_local_ip, get_os_version=get_os_version, get_ram_gb=get_ram_gb, get_storage_used=get_storage_used, get_storage_tot=get_storage_tot)
-
-if __name__ == '__main__':
-    app.run()
+data = {"data":[
+      {
+         "id": "1",
+         "maintenance": "false",
+         "ip": get_local_ip,
+         "hostname": get_hostname,
+         "os_name": get_os_name,
+         "os_version": get_os_version,
+         "uptime": "",
+         "ram_used": get_ram_gb,
+         "ram_tot": get_ram,
+         "storage_free": get_storage_free,
+         "storage_used": get_storage_used,
+         "sorage_tot": get_storage_tot,
+         "cpu": ""
+      },
+   ],}
+json_string = json.dumps(data)
