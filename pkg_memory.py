d9c4e74d3549c8784077b538bdfcb5b8a17e074f
gblnlabs.com <mail@gblnlabs.com>
2023-01-27 17:13:35 +0100

    1

--- pkg_memory.py
+++ pkg_memory.py
@@ -1,65 +1,26 @@
 import psutil
 import time
-import smtplib, ssl
-#from mail_sender import *
-
+import socket
+import time
+import datetime
 
 def checkMemory():
-    MEM_WARN_PERCENT = 80
-    MEM_CRIT_PERCENT = 90
-    SERVICE_NAME= 'Check_Memory'
-    HOST= 'localhost'
-    mail_HOST = 'ssl0.ovh.net'
-    mail_HOST_PORT = 465
-    mail_USER = 'postmaster@gblnlabs.com'
-    mail_USER_PASSWD = '#Bd36aO%$wAX'
-    mail_ACCOIUNT_SENDER = 'postmaster@gblnlabs.com'
-    mail_TO_ALERT= 'kevingatti99@gmail.com'
+    warn_PERCENT = 80
+    crit_PERCENT = 90
+    service_NAME= 'Check_Memory'
+    service_pkg=psutil.virtual_memory()[3]
+    host= socket.gethostname()
+    lan_ip= socket.gethostbyname(socket.gethostname())
+    timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-    context = ssl.create_default_context()
-    server = smtplib.SMTP_SSL(mail_HOST, mail_HOST_PORT, context=context)
-    server.login(mail_USER, mail_USER_PASSWD)
-    
-    mem_warn = (MEM_WARN_PERCENT / 100) * psutil.virtual_memory()[0]
-    mem_crit = (MEM_CRIT_PERCENT / 100) * psutil.virtual_memory()[0]
-    ##print(round(psutil.virtual_memory()[3], 1))
-    ##print('USED: ', psutil.virtual_memory()[3], ' WARN: ', mem_warn, ' Crit: ', mem_crit)
+    mem_warn = (warn_PERCENT / 100) * psutil.virtual_memory()[0]
+    mem_crit = (crit_PERCENT / 100) * psutil.virtual_memory()[0]
     
     if (psutil.virtual_memory()[3] >= mem_crit):
-        print('MEM CRIT: ', psutil.virtual_memory()[3])
-        txt = 'Memory usege @: ', psutil.virtual_memory()[3]
-        subj= SERVICE_NAME, 'Status Critical. Triggered on host: ', HOST
-        
-        message = """\
-        From: %s
-        To: %s
-        Subject: %s
-        
-        %s
-        """ % (mail_ACCOIUNT_SENDER, mail_TO_ALERT, subj, txt)
-        
-        server.sendmail(mail_ACCOIUNT_SENDER, mail_TO_ALERT, message)
-        server.quit()
-        
-        print('Mail CRIT sento TO: ', mail_TO_ALERT)
+        print(timestamp, ' Node:', host, '|', lan_ip, '=>' ,'MEM CRIT:', service_pkg)
         time.sleep(1)
     elif (psutil.virtual_memory()[3] >= mem_warn):
-        print('MEM WARN: ', psutil.virtual_memory()[3])
-        txt = 'Memory usege @: ', psutil.virtual_memory()[3]
-        subj= SERVICE_NAME, 'Status Warning. Triggered on host: ', HOST 
-        
-        message = """\
-        From: %s
-        To: %s
-        Subject: %s
-        
-        %s
-        """ % (mail_ACCOIUNT_SENDER, mail_TO_ALERT, subj, txt)
-        
-        server.sendmail(mail_ACCOIUNT_SENDER, mail_TO_ALERT, message)
-        server.quit()
-        
-        print('Mail WARN sento TO: ', mail_TO_ALERT)
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Node:', host, '|', lan_ip, '=>' , 'MEM WARN:', service_pkg)
         time.sleep(1)
     else:
-        print('MEM OK: ', psutil.virtual_memory()[3])
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Node:', host, '|', lan_ip, '=>' ,'MEM OK:', service_pkg)
